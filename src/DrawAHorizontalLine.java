a369f3cc058ba023d4feeada5f713500693ad786
MHwishes <1025383143@qq.com>
2017-07-16 19:07:03 +0800

    add Draw a horizontal line exercise

---
+++ src/DrawAHorizontalLine.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DrawAHorizontalLine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入n的值：");
+        int n = sc.nextInt();
+        int i = 0;
+        while (i < n) {
+            i++;
+            System.out.print("*");
+        }
+
+    }
+}
